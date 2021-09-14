d5bf052196c21694721816c79851bd1454abc47f
Daniel Ale <danielale9291@gmail.com>
2021-09-14 17:56:24 +0100

    add docker

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -46,10 +46,12 @@ namespace PlatformService
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlatformService v1"));
+                app.UseDeveloperExceptionPage(); 
             }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlatformService v1"));
+
 
             app.UseHttpsRedirection();
 
